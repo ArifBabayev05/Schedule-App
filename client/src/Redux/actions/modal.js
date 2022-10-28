1c5738974c55e421d5a191270e3f6909ed265ef4
Arif Babayev <arifrb@code.edu.az>
2022-10-28 16:58:52 +0400

    Schedule App

---
+++ client/src/Redux/actions/modal.js
@@ -0,0 +1,21 @@
+
+export const closeEvent =()=>{
+    return {
+        type: "CLOSE_EVENT"
+    }
+}
+
+export const  closeModal = ()=>{
+  
+   return {
+     type: "CLOSE_MODAL",
+     payload: false
+   }
+}
+
+export const  openModal = ()=>{
+    return {
+      type: "OPEN_MODAL",
+      payload: true
+    }
+ }
